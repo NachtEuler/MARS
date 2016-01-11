906635364da55e1b2e2af5e67ab3320575ced6a5
NachtEuler <NachtEuler@users.noreply.github.com>
2016-01-11 14:50:50 -0500

    Create Workspace

---
+++ src/Catalog.java
@@ -0,0 +1,4 @@
+package MARS;
+public class Catalog{
+//TODO: first task is to describe how to determine which files are copied to which destinations.
+}
---
+++ src/Client.java
@@ -0,0 +1,9 @@
+package MARS;
+
+public class Client{
+
+	public static void main(String[] args){
+		System.out.println("UNDER CONSTRUCTION");
+	}
+
+}
